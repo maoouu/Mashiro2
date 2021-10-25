aa483cbe6985c1751ee031b4c90a7f83610e7a8a
Maou <33371589+maoouu@users.noreply.github.com>
2021-10-25 10:11:21 +0000

    changed Nhentai-API to Hentai

--- cogs/nhentai.py
+++ cogs/nhentai.py
@@ -1,7 +1,9 @@
-from NHentai import NHentai
-from NHentai.entities.doujin import Doujin
+#from NHentai import NHentai
+from hentai import Hentai, Format
+from nextcord import Embed
 from nextcord.ext import commands
-from utils import default, hentai
+from utils import default#, doujintools
+
 
 
 class Nhentai(commands.Cog):
@@ -9,17 +11,24 @@ class Nhentai(commands.Cog):
         self.bot = bot
         self.config = default.config()
 
+
     # Async Commands
     @commands.command(aliases=["sc"])
     @commands.is_nsfw()
     @commands.guild_only()
-    async def decode(self, ctx, nuke_code: str):
-        """Sends an embed of the particular Doujin ID."""
+    async def decode(self, ctx, nuke_code):
+        """#Sends an embed of the particular Doujin ID."""
         message = await ctx.send(f"Retrieving `{nuke_code}`")
-        nhentai = NHentai()
-        doujin = nhentai.get_doujin(id=nuke_code)
-        # message = await ctx.send(embed=hentai.get_doujin_embed(doujin))
-        await message.edit(content="", embed=hentai.get_doujin_embed(doujin))
+        doujin = Hentai(nuke_code)
+        #nhentai = NHentai()
+        #doujin = nhentai.get_doujin(id=nuke_code)
+        embed = self.get_doujin_embed(doujin)
+        await message.edit(content="",embed=embed)
+        #await message.edit(content=doujin.title(Format.English))
+
+    def get_doujin_embed(self, doujin):
+      return Embed(title=doujin.title(Format.English), description="Test").set_image(url=doujin.cover)
+      # TODO: fix embedding of Hentai API
 
 
 def setup(bot):
--- utils/doujintools.py
+++ utils/doujintools.py
@@ -1,8 +1,9 @@
+from hentai import Hentai, Format
 from nextcord import Embed
-from NHentai.entities.doujin import Doujin
+# from NHentai.entities.doujin import Doujin
 
 
-def get_doujin_embed(doujin: Doujin):
+def get_doujin_embed(doujin):
     title = ""
     jp_title = ""
     artists = []
@@ -11,34 +12,41 @@ def get_doujin_embed(doujin: Doujin):
     tags = []
     embed = Embed()
 
-    if doujin is not None:
+    if Hentai.exists(doujin.id):
         # Gets EN title if it exists
-        if doujin.title.english:
-            title = doujin.title.english
+        if doujin.title(Format.English):
+            #title = doujin.title.english
+            title = doujin.title(Format.English)
         else:
-            title = doujin.title.japanese
+            title = doujin.title(Format.Japanese)
 
         # Gets the artists
-        if doujin.artists:
-            artists = [f"`{getattr(artist, 'name')}`" for artist in doujin.artists]
+        if doujin.artist:
+            artists = [f"`{getattr(artist, 'name')}`" for artist in doujin.artist]
             artists = ", ".join(artists)
         else:
             artists = "N/A"
 
         # Gets JP Title if it exists
-        if doujin.title.japanese:
-            jp_title = doujin.title.japanese
+        if doujin.title(Format.Japanese):
+            jp_title = doujin.title(Format.Japanese)
         else:
             jp_title = "N/A"
 
         # Gets the characters if it exists
-        if doujin.characters:
+        if doujin.character:
             characters = [
-                f"`{getattr(character, 'name')}`" for character in doujin.characters
+                f"`{getattr(character, 'name')}`" for character in doujin.character
             ]
             characters = ", ".join(characters)
         else:
             characters = "N/A"
+        
+        # Gets the group that made the doujinshi
+        if doujin.group:
+            groups = [f"`{getattr(group, 'name')}`" for group in doujin.group]
+        else:
+            groups = "N/A"
 
         # Collect all of the doujin's tags
         if doujin.tags:
@@ -50,14 +58,15 @@ def get_doujin_embed(doujin: Doujin):
         description = f""" ID: `{doujin.id}`
             Japanese Title: `{jp_title}`
             Artists: {artists}
+            Groups: {groups}
             Characters: {characters}
             Pages: `{doujin.total_pages}`
             Tags: {tags} """
 
         # Assemble the embed
-        embed.set_thumbnail(url=doujin.cover.src)
-        embed.set_image(url=doujin.cover.src)
-        embed.set_footer(text=f"Uploaded at {doujin.upload_at.strftime('%d-%m-%Y')}")
+        embed.set_thumbnail(url=doujin.cover)
+        embed.set_image(url=doujin.cover)
+        embed.set_footer(text=f"Uploaded at {doujin.upload_date.strftime('%d-%m-%Y')}")
     else:
         title = "Error"
         description = "The doujin you are looking for does not exist."
