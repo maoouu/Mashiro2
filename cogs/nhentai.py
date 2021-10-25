dd49977e52d6fb65bfbb8672f662b04db4f77a6d
Maou <33371589+maoouu@users.noreply.github.com>
2021-10-25 10:28:11 +0000

    update(10.25.2021)

--- cogs/nhentai.py
+++ cogs/nhentai.py
@@ -19,15 +19,38 @@ class Nhentai(commands.Cog):
     async def decode(self, ctx, nuke_code):
         """#Sends an embed of the particular Doujin ID."""
         message = await ctx.send(f"Retrieving `{nuke_code}`")
-        doujin = Hentai(nuke_code)
+        #doujin = Hentai(nuke_code)
         #nhentai = NHentai()
         #doujin = nhentai.get_doujin(id=nuke_code)
-        embed = self.get_doujin_embed(doujin)
+        embed = self.get_doujin_embed(nuke_code)
         await message.edit(content="",embed=embed)
         #await message.edit(content=doujin.title(Format.English))
 
-    def get_doujin_embed(self, doujin):
-      return Embed(title=doujin.title(Format.English), description="Test").set_image(url=doujin.cover)
+    def get_doujin_embed(self, nuke_code):
+        doujin = ""
+        title = ""
+        jp_title = ""
+        artists = []
+        characters = []
+        groups = []
+        tags = []
+        embed = Embed()
+
+        if Hentai.exists(nuke_code):
+          doujin = Hentai(nuke_code)
+          title = "Test"
+          description = "It Works!"
+        else:
+          title = "Error"
+          description = "The doujin you are looking for does not exist"
+        
+        embed = Embed(title=title, description=description)
+        embed.set_author(
+            name="NHentai",
+            icon_url="https://clipground.com/images/nhentai-logo-3.jpg"
+        )
+        return embed
+      #return Embed(title=doujin.title(Format.English), description="Test").set_image(url=doujin.cover)
       # TODO: fix embedding of Hentai API
 
 
