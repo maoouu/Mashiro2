aa2956ada00fceb9376dfa7d587a27b603d0b5a7
Maou <33371589+maoouu@users.noreply.github.com>
2021-11-15 06:01:51 +0000

    gitignore certain files

--- cogs/test.py
+++ cogs/test.py
@@ -1,3 +1,4 @@
+from nextcord import ui
 from nextcord.ext import commands, menus
 #from nextcord.ext.menus import MenuPaginationButton
 from utils import default
@@ -19,6 +20,30 @@ class ReactionMenu(menus.Menu):
         self.stop()
         await self.message.delete()
 
+class ReaderMenu(menus.ButtonMenu):
+    def __init__(self):
+        super().__init__(timeout=10,disable_buttons_after=True)
+
+    async def send_initial_message(self, ctx, channel):
+        return await channel.send(f"Hello {ctx.author}", view=self)
+    
+    @ui.button(emoji='\N{THUMBS UP SIGN}')
+    async def on_thumbs_up(self, button, interaction):
+        await self.message.edit(content=f"Thanks {interaction.user}!")
+    
+    @ui.button(emoji='\N{BLACK SQUARE FOR STOP}\ufe0f')
+    async def on_stop(self, button, interaction):
+        await self.message.edit(content="Reader has been stopped.")
+        self.stop()
+    
+    @ui.button(emoji='\N{THUMBS DOWN SIGN}')
+    async def on_thumbs_down(self, button, interaction):
+        await self.message.edit(content="Ohh... okay.")
+    
+    async def on_timeout(self):
+        await self.message.edit(content="Reader has timed out.")
+        self.stop()
+
 
 class Test(commands.Cog):
     def __init__(self, bot):
@@ -29,6 +54,11 @@ class Test(commands.Cog):
     @commands.is_owner()
     async def reaction_buttons(self, ctx):
         await ReactionMenu().start(ctx)
+    
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def discord_buttons(self, ctx):
+        await ReaderMenu().start(ctx)
 
         
 def setup(bot):
